ea2c66229100ddc1afce47801d20854ec4bca981
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-06-24 17:09:55 -0600

    paperclip images

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -76,6 +76,6 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:name, :price, :description, :category_id, :wage)
+      params.require(:listing).permit(:name, :price, :description, :category_id, :wage, :image)
     end
 end
